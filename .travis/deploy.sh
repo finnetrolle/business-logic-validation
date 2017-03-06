3d410e033cdbc47c6fdaf9d9c68b65575fcb6834
finnetrolle <maxsyachin@gmail.com>
2017-03-07 02:03:23 +0300

    fixing deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-cd `dirname $0`/..
-
-if [ -z "$SONATYPE_USERNAME" ]
-then
-    echo "error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
-    exit 1
-fi
-
-if [ -z "$SONATYPE_PASSWORD" ]
-then
-    echo "error: please set SONATYPE_PASSWORD environment variable"
-    exit 1
-fi
-
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
